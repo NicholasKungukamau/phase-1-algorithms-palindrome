8c2f59329fc012fd70357e9c1a8b70308af0e29f
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-17 17:56:55 +0300

    commit tree 98262282d8e074db0a50ca6368a8f276600a5ae0

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
 */
+// steps
+// 1. Make function called isPalindrome.
+// 2. Make constant called wordArray.
+// 3. Split our word using .split('')  and assign to wordArray.
+// 4. Use .reverse() to reverse our word.
+// 5. Assign the reversed word to new const called reverseWordArray.
+// 7. Return joined array.
+// 6. Compare joined array to word string using strict equality getOperator.
+
+
 
 /*
   Add written explanation of your solution here
 */
+// Use java script inbuilt function string.reverse() to change our string to array and compare each individual characters in string  using strict opeator to make sure they form word which is palindrome and return the compared characters as string by using join(' ') method.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +49,33 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+   
+console.log("");
+
+ console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+   console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+   console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+   console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+   console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
